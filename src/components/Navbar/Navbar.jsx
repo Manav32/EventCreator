2ad969604affd7d9d2485bd334c938a37768fd5e
Manav32 <vermamanav117@gmail.com>
2022-10-04 23:05:04 +0530

    company name added in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Andrew</div>
+        <div className="n-name">Event Creator</div>
         <Toggle />
       </div>
       {/* right */}
